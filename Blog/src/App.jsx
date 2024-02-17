f65df53e3de274b4c2c92bc65e702e830bea5552
sthasaroj13 <sarojshrestha651@gmail.com>
2024-02-17 19:45:13 +0545

    making components which can reuse again and again

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,14 +1,49 @@
 
+import { useState,useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import Header from './components/header/Header'
+import Footer from './components/Footer/Footer'
+
+
+
+
 import './App.css'
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL);
+// console.log(import.meta.env.VITE_APPWRITE_URL);
+const[loading,setLoading] = useState(true)
+const  dispatch = useDispatch()
+useEffect(()=>{
+  authService.getCurrentUser()
+  .then((userData)=>{
+    if (userData) {
+      dispatch(login(userData))
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>setLoading(false))
 
-  return (
+},[])
+
+  return !loading? (
     <>
-     <h1>mega project</h1>
+     <div className=' min-h-screen flex flex-wrap content-between bg-gray-400'>
+
+      <div className=' w-full  block'>
+        <Header/>
+        <main>
+          TODO  {/* <OutLet/> */}
+        </main>
+        <Footer/>
+
+      </div>
+     </div>
     </>
-  )
+  ):null
 }
 
 export default App
---
+++ Blog/src/components/index.js
@@ -0,0 +1,16 @@
+import Header from "./header/Header";
+ import Footer from "./Footer/Footer";
+ import Container from "./Container/Container";
+ import Logo from "./Logo";
+ import LogoutBtn from "./header/LogoutBtn";
+ import Button from './Button'
+ import Input from './Input'
+  export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+  }
